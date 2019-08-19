ed9db8f1fa0c54c8ca5cf5c18f5bb0d4726c74d6
Darren Smith <contact@darrensmith.com.au>
2019-08-20 09:18:00 +1000

    MH-39 Video Clips Listing Page

---
+++ src/services/mediahub/controllers/web/video-clips/controller.js
@@ -0,0 +1,65 @@
+/*!
+* /web/video-clips/controller.js
+*
+* Copyright (c) 2019 Darren Smith
+* Licensed under the LGPL license.
+*/
+
+;!function(undefined) {
+
+	var ctrl = {};
+	var isnode = null;
+	var service = null;
+	var SoundByteModel = null;
+
+	/**
+	 * Initialises the controller
+	 * @param {object} isnodeObj - The parent isnode object
+	 */
+	ctrl.init = function(isnodeObj){
+		isnode = isnodeObj;
+		service = isnode.module("services").service("mediahub");
+		VideoClipModel = service.models.get("videoClip");
+		return;
+	}
+
+	/**
+	 * GET
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.get = function(req, res){
+		var context = {};
+		context.backButtonLink = "/web";
+		VideoClipModel.find({ "where": { }}, function(err, videoClips){
+			context.videoClips = videoClips;
+			var leftnav = require("../../../lib/leftnav.js");
+			leftnav(isnode, context, function(err, cxt){
+				res.render("video-clips.mustache", cxt);
+			});
+		});
+		return;
+	}
+
+	/**
+	 * Shutdown Controller
+	 * @param {function} cb - Callback function
+	 */
+	ctrl.shutdown = function(cb) {
+		cb();
+	}
+
+	/**
+	 * (ENTRY POINT FOR EXECUTION)
+	 *
+	 * Alter module behaviour based on execution use case: 
+	 *
+	 * (i) Attempt to Open Command-Line Interface (CLI) - INVALID; or
+	 * (ii) Export as Node.JS module for inclusion in another application - VALID
+	 */
+	if (!module.parent) { 
+		console.log("ERROR: Controller cannot be executed independently as it is an isnode application library only.");
+	} else { 
+		module.exports = ctrl;
+	}
+}();
