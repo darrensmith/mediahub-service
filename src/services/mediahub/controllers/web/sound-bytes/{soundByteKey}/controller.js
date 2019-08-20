5731267527ba3bbce84a19802c24abdd2641435f
Darren Smith <contact@darrensmith.com.au>
2019-08-20 14:13:38 +1000

    MH-50 Sound Byte Details Page MH-80 Revert Sound Byte to File

---
+++ src/services/mediahub/controllers/web/sound-bytes/{soundByteKey}/controller.js
@@ -0,0 +1,111 @@
+/*!
+* /web/sound-bytes/{soundByteKey}/controller.js
+*
+* Copyright (c) 2019 Darren Smith
+* Licensed under the LGPL license.
+*/
+
+;!function(undefined) {
+
+	var ctrl = {};
+	var isnode = null;
+	var service = null;
+	var SoundByteModel = null;
+	var FileModel = null;
+
+	/**
+	 * Initialises the controller
+	 * @param {object} isnodeObj - The parent isnode object
+	 */
+	ctrl.init = function(isnodeObj){
+		isnode = isnodeObj;
+		service = isnode.module("services").service("mediahub");
+		SoundByteModel = service.models.get("soundByte");
+		FileModel = service.models.get("file");
+		return;
+	}
+
+	/**
+	 * GET
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.get = function(req, res){
+		var context = {};
+		context.backButtonLink = "/web/sound-bytes";
+		SoundByteModel.find({ "where": { key: req.params.soundByteKey }}, function(err,soundBytes){
+			context.soundByte = soundBytes[0];
+			var leftnav = require("../../../../lib/leftnav.js");
+			leftnav(isnode, context, function(err, cxt){
+				res.render("sound-byte-details.mustache", cxt);
+			});
+		});
+		return;
+	}
+
+	/**
+	 * POST
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.post = function(req, res){
+		var context = {};
+		SoundByteModel.find({ "where": { key: req.params.soundByteKey }}, function(err1, soundBytes){
+			if(!soundBytes || err1 || !soundBytes[0]){
+				res.redirect("/web/sound-bytes");
+				return;
+			}
+			FileModel.find({ "where": { key: soundBytes[0].fileKey }}, function(err2, files){
+				if(!files || err2 || !files[0]){
+					res.redirect("/web/sound-bytes");
+					return;
+				}
+				if(req.body.revert == "true"){
+					var completed = 0;
+					soundBytes[0].destroy(function(err3, deletedSoundByte){
+						completed ++;
+					});
+					FileModel.update({ 
+						where: { key: soundBytes[0].fileKey } 
+					}, {
+						objectType: null,
+						objectKey: null
+					}, function(err4, updatedFile){
+						completed ++;
+					});
+					var interval = setInterval(function(){
+						if(completed >= 2){
+							clearInterval(interval);
+							res.redirect("/web/files");
+						}
+					}, 200);
+				} else if (req.body.edit == "true") {
+					res.redirect("/web/sound-bytes/" + req.params.soundByteKey + "/edit");
+				}			
+			});
+		});
+		return;
+	}
+
+	/**
+	 * Shutdown Controller
+	 * @param {function} cb - Callback function
+	 */
+	ctrl.shutdown = function(cb) {
+		cb();
+	}
+
+	/**
+	 * (ENTRY POINT FOR EXECUTION)
+	 *
+	 * Alter module behaviour based on execution use case: 
+	 *
+	 * (i) Attempt to Open Command-Line Interface (CLI) - INVALID; or
+	 * (ii) Export as Node.JS module for inclusion in another application - VALID
+	 */
+	if (!module.parent) { 
+		console.log("ERROR: Controller cannot be executed independently as it is an isnode application library only.");
+	} else { 
+		module.exports = ctrl;
+	}
+}();
