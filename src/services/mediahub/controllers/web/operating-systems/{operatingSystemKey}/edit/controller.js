84453c53ce7b78e562a1673c6ed67195d07c3ba1
Darren Smith <contact@darrensmith.com.au>
2019-08-20 20:56:02 +1000

    MH-74 Operating System Edit Page + Fix to listing page for operating systems (removed author - as its unused)

---
+++ src/services/mediahub/controllers/web/operating-systems/{operatingSystemKey}/edit/controller.js
@@ -0,0 +1,75 @@
+/*!
+* /web/operating-systems/{operatingSystemKey}/edit/controller.js
+*
+* Copyright (c) 2019 Darren Smith
+* Licensed under the LGPL license.
+*/
+
+;!function(undefined) {
+
+	var ctrl = {};
+	var isnode = null;
+	var service = null;
+	var OperatingSystemModel = null;
+
+	/**
+	 * Initialises the controller
+	 * @param {object} isnodeObj - The parent isnode object
+	 */
+	ctrl.init = function(isnodeObj){
+		isnode = isnodeObj;
+		service = isnode.module("services").service("mediahub");
+		OperatingSystemModel = service.models.get("operatingSystem");
+		return;
+	}
+
+	/**
+	 * GET
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.get = function(req, res){
+		var context = {};
+		context.backButtonLink = "/web/operating-systems/" + req.params.operatingSystemKey;
+		OperatingSystemModel.find({ "where": { key: req.params.operatingSystemKey }}, function(err, operatingSystems){
+			context.operatingSystem = operatingSystems[0];
+			var leftnav = require("../../../../../lib/leftnav.js");
+			leftnav(isnode, context, function(err, cxt){
+				res.render("operating-system-edit.mustache", cxt);
+			});
+		});
+		return;
+	}
+
+	/**
+	 * POST
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.post = function(req, res){
+		res.redirect("/web/operating-systems/" + req.params.operatingSystemKey + "/edit");
+		return;
+	}
+
+	/**
+	 * Shutdown Controller
+	 * @param {function} cb - Callback function
+	 */
+	ctrl.shutdown = function(cb) {
+		cb();
+	}
+
+	/**
+	 * (ENTRY POINT FOR EXECUTION)
+	 *
+	 * Alter module behaviour based on execution use case: 
+	 *
+	 * (i) Attempt to Open Command-Line Interface (CLI) - INVALID; or
+	 * (ii) Export as Node.JS module for inclusion in another application - VALID
+	 */
+	if (!module.parent) { 
+		console.log("ERROR: Controller cannot be executed independently as it is an isnode application library only.");
+	} else { 
+		module.exports = ctrl;
+	}
+}();
