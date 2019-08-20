7052bb70c9fcaa38e53ebc2ee286eb45062e43d1
Darren Smith <contact@darrensmith.com.au>
2019-08-20 18:25:28 +1000

    MH-59 Operating System Details Page MH-89 Revert Operating System to File

---
+++ src/services/mediahub/controllers/web/operating-systems/{operatingSystemKey}/controller.js
@@ -0,0 +1,111 @@
+/*!
+* /web/operating-systems/{operatingSystemKey}/controller.js
+*
+* Copyright (c) 2019 Darren Smith
+* Licensed under the LGPL license.
+*/
+
+;!function(undefined) {
+
+	var ctrl = {};
+	var isnode = null;
+	var service = null;
+	var OperatingSystemModel = null;
+	var FileModel = null;
+
+	/**
+	 * Initialises the controller
+	 * @param {object} isnodeObj - The parent isnode object
+	 */
+	ctrl.init = function(isnodeObj){
+		isnode = isnodeObj;
+		service = isnode.module("services").service("mediahub");
+		OperatingSystemModel = service.models.get("operatingSystem");
+		FileModel = service.models.get("file");
+		return;
+	}
+
+	/**
+	 * GET
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.get = function(req, res){
+		var context = {};
+		context.backButtonLink = "/web/operating-systems";
+		OperatingSystemModel.find({ "where": { key: req.params.operatingSystemKey }}, function(err,operatingSystems){
+			context.operatingSystem = operatingSystems[0];
+			var leftnav = require("../../../../lib/leftnav.js");
+			leftnav(isnode, context, function(err, cxt){
+				res.render("operating-system-details.mustache", cxt);
+			});
+		});
+		return;
+	}
+
+	/**
+	 * POST
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.post = function(req, res){
+		var context = {};
+		OperatingSystemModel.find({ "where": { key: req.params.operatingSystemKey }}, function(err1, operatingSystems){
+			if(!operatingSystems || err1 || !operatingSystems[0]){
+				res.redirect("/web/operating-systems");
+				return;
+			}
+			FileModel.find({ "where": { key: operatingSystems[0].fileKey }}, function(err2, files){
+				if(!files || err2 || !files[0]){
+					res.redirect("/web/operating-systems");
+					return;
+				}
+				if(req.body.revert == "true"){
+					var completed = 0;
+					operatingSystems[0].destroy(function(err3, deletedOperatingSystem){
+						completed ++;
+					});
+					FileModel.update({ 
+						where: { key: operatingSystems[0].fileKey } 
+					}, {
+						objectType: null,
+						objectKey: null
+					}, function(err4, updatedFile){
+						completed ++;
+					});
+					var interval = setInterval(function(){
+						if(completed >= 2){
+							clearInterval(interval);
+							res.redirect("/web/files");
+						}
+					}, 200);
+				} else if (req.body.edit == "true") {
+					res.redirect("/web/operating-systems/" + req.params.operatingSystemKey + "/edit");
+				}			
+			});
+		});
+		return;
+	}
+
+	/**
+	 * Shutdown Controller
+	 * @param {function} cb - Callback function
+	 */
+	ctrl.shutdown = function(cb) {
+		cb();
+	}
+
+	/**
+	 * (ENTRY POINT FOR EXECUTION)
+	 *
+	 * Alter module behaviour based on execution use case: 
+	 *
+	 * (i) Attempt to Open Command-Line Interface (CLI) - INVALID; or
+	 * (ii) Export as Node.JS module for inclusion in another application - VALID
+	 */
+	if (!module.parent) { 
+		console.log("ERROR: Controller cannot be executed independently as it is an isnode application library only.");
+	} else { 
+		module.exports = ctrl;
+	}
+}();
