8904c7d4e8cc93a68eaf2d0bf98e75cebeb84aa8
Darren Smith <contact@darrensmith.com.au>
2019-08-20 20:34:43 +1000

    MH-69 Music Video Edit Page

---
+++ src/services/mediahub/controllers/web/music-videos/{musicVideoKey}/edit/controller.js
@@ -0,0 +1,75 @@
+/*!
+* /web/music-videos/{musicVideoKey}/edit/controller.js
+*
+* Copyright (c) 2019 Darren Smith
+* Licensed under the LGPL license.
+*/
+
+;!function(undefined) {
+
+	var ctrl = {};
+	var isnode = null;
+	var service = null;
+	var SongModel = null;
+
+	/**
+	 * Initialises the controller
+	 * @param {object} isnodeObj - The parent isnode object
+	 */
+	ctrl.init = function(isnodeObj){
+		isnode = isnodeObj;
+		service = isnode.module("services").service("mediahub");
+		MusicVideoModel = service.models.get("musicVideo");
+		return;
+	}
+
+	/**
+	 * GET
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.get = function(req, res){
+		var context = {};
+		context.backButtonLink = "/web/music-videos/" + req.params.musicVideoKey;
+		MusicVideoModel.find({ "where": { key: req.params.musicVideoKey }}, function(err, musicVideos){
+			context.musicVideo = musicVideos[0];
+			var leftnav = require("../../../../../lib/leftnav.js");
+			leftnav(isnode, context, function(err, cxt){
+				res.render("music-video-edit.mustache", cxt);
+			});
+		});
+		return;
+	}
+
+	/**
+	 * POST
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.post = function(req, res){
+		res.redirect("/web/music-videos/" + req.params.musicVideoKey + "/edit");
+		return;
+	}
+
+	/**
+	 * Shutdown Controller
+	 * @param {function} cb - Callback function
+	 */
+	ctrl.shutdown = function(cb) {
+		cb();
+	}
+
+	/**
+	 * (ENTRY POINT FOR EXECUTION)
+	 *
+	 * Alter module behaviour based on execution use case: 
+	 *
+	 * (i) Attempt to Open Command-Line Interface (CLI) - INVALID; or
+	 * (ii) Export as Node.JS module for inclusion in another application - VALID
+	 */
+	if (!module.parent) { 
+		console.log("ERROR: Controller cannot be executed independently as it is an isnode application library only.");
+	} else { 
+		module.exports = ctrl;
+	}
+}();
