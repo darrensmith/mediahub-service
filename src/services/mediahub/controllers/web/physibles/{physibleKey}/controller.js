2a51adce9598d6806cf50920caea3b4d817a8c05
Darren Smith <contact@darrensmith.com.au>
2019-08-20 18:31:21 +1000

    MH-60 Physible Details Page MH-90 Revert Physible to File

---
+++ src/services/mediahub/controllers/web/physibles/{physibleKey}/controller.js
@@ -0,0 +1,111 @@
+/*!
+* /web/physibles/{physibleKey}/controller.js
+*
+* Copyright (c) 2019 Darren Smith
+* Licensed under the LGPL license.
+*/
+
+;!function(undefined) {
+
+	var ctrl = {};
+	var isnode = null;
+	var service = null;
+	var PhysibleModel = null;
+	var FileModel = null;
+
+	/**
+	 * Initialises the controller
+	 * @param {object} isnodeObj - The parent isnode object
+	 */
+	ctrl.init = function(isnodeObj){
+		isnode = isnodeObj;
+		service = isnode.module("services").service("mediahub");
+		PhysibleModel = service.models.get("physible");
+		FileModel = service.models.get("file");
+		return;
+	}
+
+	/**
+	 * GET
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.get = function(req, res){
+		var context = {};
+		context.backButtonLink = "/web/physibles";
+		PhysibleModel.find({ "where": { key: req.params.physibleKey }}, function(err,physibles){
+			context.physible = physibles[0];
+			var leftnav = require("../../../../lib/leftnav.js");
+			leftnav(isnode, context, function(err, cxt){
+				res.render("physible-details.mustache", cxt);
+			});
+		});
+		return;
+	}
+
+	/**
+	 * POST
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.post = function(req, res){
+		var context = {};
+		PhysibleModel.find({ "where": { key: req.params.physibleKey }}, function(err1, physibles){
+			if(!physibles || err1 || !physibles[0]){
+				res.redirect("/web/physibles");
+				return;
+			}
+			FileModel.find({ "where": { key: physibles[0].fileKey }}, function(err2, files){
+				if(!files || err2 || !files[0]){
+					res.redirect("/web/physibles");
+					return;
+				}
+				if(req.body.revert == "true"){
+					var completed = 0;
+					physibles[0].destroy(function(err3, deletedPhysible){
+						completed ++;
+					});
+					FileModel.update({ 
+						where: { key: physibles[0].fileKey } 
+					}, {
+						objectType: null,
+						objectKey: null
+					}, function(err4, updatedFile){
+						completed ++;
+					});
+					var interval = setInterval(function(){
+						if(completed >= 2){
+							clearInterval(interval);
+							res.redirect("/web/files");
+						}
+					}, 200);
+				} else if (req.body.edit == "true") {
+					res.redirect("/web/physibles/" + req.params.physibleKey + "/edit");
+				}			
+			});
+		});
+		return;
+	}
+
+	/**
+	 * Shutdown Controller
+	 * @param {function} cb - Callback function
+	 */
+	ctrl.shutdown = function(cb) {
+		cb();
+	}
+
+	/**
+	 * (ENTRY POINT FOR EXECUTION)
+	 *
+	 * Alter module behaviour based on execution use case: 
+	 *
+	 * (i) Attempt to Open Command-Line Interface (CLI) - INVALID; or
+	 * (ii) Export as Node.JS module for inclusion in another application - VALID
+	 */
+	if (!module.parent) { 
+		console.log("ERROR: Controller cannot be executed independently as it is an isnode application library only.");
+	} else { 
+		module.exports = ctrl;
+	}
+}();
