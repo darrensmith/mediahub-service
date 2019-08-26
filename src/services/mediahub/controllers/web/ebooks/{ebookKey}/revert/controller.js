809b60f0d9550dde530130a32298e0c3199284bd
Darren Smith <contact@darrensmith.com.au>
2019-08-27 00:55:05 +1000

    MH-393 Move Download, Edit & Revert to File Buttons Into Title Strip for eBooks

---
+++ src/services/mediahub/controllers/web/ebooks/{ebookKey}/revert/controller.js
@@ -0,0 +1,88 @@
+/*!
+* /web/ebooks/{ebookKey}/revert/controller.js
+*
+* Copyright (c) 2019 Darren Smith
+* Licensed under the LGPL license.
+*/
+
+;!function(undefined) {
+
+	var ctrl = {};
+	var isnode = null;
+	var service = null;
+	var eBookModel = null;
+	var FileModel = null;
+
+	/**
+	 * Initialises the controller
+	 * @param {object} isnodeObj - The parent isnode object
+	 */
+	ctrl.init = function(isnodeObj){
+		isnode = isnodeObj;
+		service = isnode.module("services").service("mediahub");
+		FileModel = service.models.get("file");
+		eBookModel = service.models.get("ebook");
+		return;
+	}
+
+	/**
+	 * GET
+	 * @param {object} req - Request object
+	 * @param {object} res - Response object
+	 */
+	ctrl.get = function(req, res){
+		eBookModel.find({ "where": { key: req.params.ebookKey }}, function(err1, eBooks){
+			if(!eBooks){
+				res.redirect("/web/ebooks?message=ebook-not-found");
+				return;
+			}
+			FileModel.find({ "where": { key: eBooks[0].fileKey }}, function(err2, files){
+				if(!files){
+					res.redirect("/web/ebooks?message=file-not-found");
+					return;
+				}
+				var completed = 0;
+				eBooks[0].destroy(function(err3, deletedEBook){
+					completed ++;
+				});
+				FileModel.update({ 
+					where: { key: eBooks[0].fileKey } 
+				}, {
+					objectType: null,
+					objectKey: null
+				}, function(err4, updatedFile){
+					completed ++;
+				});
+				var interval = setInterval(function(){
+					if(completed >= 2){
+						clearInterval(interval);
+						res.redirect("/web/ebooks");
+					}
+				}, 200);		
+			});
+		});
+		return;
+	}
+
+	/**
+	 * Shutdown Controller
+	 * @param {function} cb - Callback function
+	 */
+	ctrl.shutdown = function(cb) {
+		cb();
+	}
+
+	/**
+	 * (ENTRY POINT FOR EXECUTION)
+	 *
+	 * Alter module behaviour based on execution use case: 
+	 *
+	 * (i) Attempt to Open Command-Line Interface (CLI) - INVALID; or
+	 * (ii) Export as Node.JS module for inclusion in another application - VALID
+	 */
+	if (!module.parent) { 
+		console.log("ERROR: Controller cannot be executed independently as it is an isnode application library only.");
+	} else { 
+		module.exports = ctrl;
+	}
+}();
